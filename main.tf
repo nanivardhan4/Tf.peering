7711d10ec2e37520972695411c669eb0a35b10a2
nanivardhan <nanivardhan4@gmail.com>
2024-12-23 10:31:17 +0530

    second change

--- main.tf
+++ main.tf
@@ -7,4 +7,11 @@ resource "aws_vpc" "vpc_a" {
 resource "aws_vpc" "vpc_b" {
   cidr_block = "10.1.0.0/16"
 }
-
+resource "aws_vpc_peering_connection" "peer_connection" {
+vpc_id = aws_vpc.vpc_a.id
+peer_vpc_id = aws_vpc.vpc_b.id
+auto_accept = true
+tags = {
+Name = "aws-vpc-peer"
+}
+}
